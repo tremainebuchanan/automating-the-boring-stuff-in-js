9783dc4ca39174ea11a47e74a379d93af22cc563
Tremaine Buchanan <tremainekbuchanan@gmail.com>
2020-08-16 18:05:31 -0500

    chore: removal of hardcoded monitor switch logic

--- monitor-config/index.js
+++ monitor-config/index.js
@@ -13,33 +13,29 @@ async function start(){
     let list = [];
     // get the list of connected devices
     const len = displays.length;
+    let displayDetails = []
     for(let i = 0; i < len; i++){
         if(displays[i].indexOf('connected') > 1){
             if(displays[i+1].indexOf('connected') === -1){
-                console.log(displays[i+1])
+                if(displays[i+1] !== '')
+                    displayDetails.push(displays[i+1].split(/\s+/g))
             }
         }
     }
-    // for(let display of displays){
-    //     if(display.indexOf('disconnected') === -1 && display !== ''){
-    //         list.push(display)
-    //     }
-    // }
-    // console.log(displays)
-    // if(list[0].indexOf('primary') > 1){
-    //     console.log('primary display active')
-    //     console.log(`switching to ${list[1]}`)
+    for(let display of displays){
+        if(display.indexOf('connected') > 1 && display !== ''){
+            list.push(display)
+        }
+    }
+
+    if(list[0].indexOf('primary') > 1){
+        console.log('primary display active.switching to larger display')
+        const command = generateCommand(list, displayDetails)
+        execCommand(command)
  
-    // }else{
-    //     console.log('primary display not active. no need to switch')        
-    // }
-
-    //const { ste, std } = await execCommand(commands["detailed"]);
-    //console.log(std)
-
-    // const { ste, std } = await execCommand(commands["connected"]);
-    // const details = std.split(/\r\n|\r|\n/g);
-    // console.log(details)
+    }else{
+        console.log('primary display not active. no need to switch')        
+    }
 }
 
 start()
@@ -50,6 +46,28 @@ async function execCommand(command){
     return { stderr, stdout }
 }
 
-// get the current monitor xrandr --listmonitors
-// if the current monitor is built in, 
-//      switch to other monitor
+function generateCommand(displays, connectedDisplayDetails){
+    let displayIds= [];
+    let displayValues = [];
+    let configs = [];
+    for(let display of displays){
+        let str;
+        str = display.split(/\s+/g);
+        displayIds.push(str[0]);
+    }
+    const displayDetails = connectedDisplayDetails.flat();
+
+    for( let detail of displayDetails){
+        if(detail !== "") displayValues.push(detail)
+    }
+    for(let j = 0; j < displayValues.length; j++){
+        let value = displayValues[j];
+        if(value.includes("x")){
+            configs.push(value);
+            configs.push(displayValues[j+1].slice(0,5));
+        }
+    }
+    const command = `xrandr --output ${displayIds[1]} --primary --mode ${configs[2]} --rate ${configs[3]} --output ${displayIds[0]} --off`;
+    return command;
+
+}
