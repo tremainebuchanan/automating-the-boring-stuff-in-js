fb537f50accd952b7184ca0e0390cfe5f84d76f7
Tremaine Buchanan <tremainekbuchanan@gmail.com>
2020-08-16 15:51:11 -0500

    feat:detecting monitors

--- monitor-config/index.js
+++ monitor-config/index.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env node
 'use strict';
 const { exec } = require('child_process');
-exec('xrandr --output HDMI-1 --primary --mode 1920x1080 --rate 60.00 --output eDP-1 --off', (error, stdout, stderr) => {
+exec('xrandr -q | grep "connected"', (error, stdout, stderr) => {
     if(error) {
         console.error(`exec error: ${error}`)
         return;
     }
     if(stderr) console.error(`${stderr}`);
+    const lines = stdout.split(/\r\n|\r|\n/g);
+    if(lines[0].indexOf('primary') > 1){
+        console.log('primary display active')
+        console.log(`switching to ${lines[1]}`)
+    }else{
+        console.log('primary display not active')
+        
+    }
+    //console.log(stdout);
 });
+
+// get the current monitor xrandr --listmonitors
+// if the current monitor is built in, 
+//      switch to other monitor
